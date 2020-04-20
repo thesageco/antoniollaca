b83a662f13202d340bf0d787eacca6895980501b
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2020-04-19 21:03:57 -0400

    Updates

---
+++ precache-manifest.06ed6a33a55b39c7d422e86adc7a3e64.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192f35700fafa2896b007328ecab951d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31897521711a692ab4b9",
+    "url": "/static/css/main.8f64d80e.chunk.css"
+  },
+  {
+    "revision": "5935dc4147f0d150c541",
+    "url": "/static/js/2.3e47353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e47353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31897521711a692ab4b9",
+    "url": "/static/js/main.3f5cea9d.chunk.js"
+  },
+  {
+    "revision": "554c4c7b40f0087fb9ff",
+    "url": "/static/js/runtime-main.53366327.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "cd7c3e4fd9bc660daabb273bb173aaae",
+    "url": "/static/media/down-arrow.cd7c3e4f.svg"
+  },
+  {
+    "revision": "6494aad0999a59446aa97777c4304b29",
+    "url": "/static/media/facebook.6494aad0.svg"
+  },
+  {
+    "revision": "99e3e6c893f2f6dc449352d225873219",
+    "url": "/static/media/instagram.99e3e6c8.svg"
+  },
+  {
+    "revision": "20e1565dd5c9e47bd241d99c0dd55ad1",
+    "url": "/static/media/playfair.20e1565d.ttf"
+  },
+  {
+    "revision": "9546889da554425a79fe4a6eaf186837",
+    "url": "/static/media/twitter.9546889d.svg"
+  }
+]);
