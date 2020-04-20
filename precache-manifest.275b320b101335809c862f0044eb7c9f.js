5489a4860430ffb5fd3c32ef7924a4f2f1202ee2
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2020-04-19 22:25:31 -0400

    Updates

---
+++ precache-manifest.275b320b101335809c862f0044eb7c9f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6262ebfc6c9bf1a31122cf2de4b699",
+    "url": "/antoniollaca/index.html"
+  },
+  {
+    "revision": "68ef0d28dcd5fec4676f",
+    "url": "/antoniollaca/static/css/main.07fe9acf.chunk.css"
+  },
+  {
+    "revision": "7cc58e7fe8c37623a8b8",
+    "url": "/antoniollaca/static/js/2.5aef673d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/antoniollaca/static/js/2.5aef673d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ef0d28dcd5fec4676f",
+    "url": "/antoniollaca/static/js/main.2fd7e1df.chunk.js"
+  },
+  {
+    "revision": "f93ae5b1ce9053d0a6e2",
+    "url": "/antoniollaca/static/js/runtime-main.6e360e44.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/antoniollaca/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/antoniollaca/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/antoniollaca/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "cd7c3e4fd9bc660daabb273bb173aaae",
+    "url": "/antoniollaca/static/media/down-arrow.cd7c3e4f.svg"
+  },
+  {
+    "revision": "6494aad0999a59446aa97777c4304b29",
+    "url": "/antoniollaca/static/media/facebook.6494aad0.svg"
+  },
+  {
+    "revision": "99e3e6c893f2f6dc449352d225873219",
+    "url": "/antoniollaca/static/media/instagram.99e3e6c8.svg"
+  },
+  {
+    "revision": "20e1565dd5c9e47bd241d99c0dd55ad1",
+    "url": "/antoniollaca/static/media/playfair.20e1565d.ttf"
+  },
+  {
+    "revision": "9546889da554425a79fe4a6eaf186837",
+    "url": "/antoniollaca/static/media/twitter.9546889d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06ed6a33a55b39c7d422e86adc7a3e64.js"
+  "/antoniollaca/precache-manifest.275b320b101335809c862f0044eb7c9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/antoniollaca/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
